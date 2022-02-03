266c83269ffb869a6dd19df503e45d270f9f6b7f
diegodai <diego.daiuto@socialpath.com.ar>
2022-02-03 09:42:23 -0300

    Pagina New Batch - Creacion Lotes Dinamica

--- lib/app_state.dart
+++ lib/app_state.dart
@@ -36,6 +36,8 @@ class AppState extends ChangeNotifier {
   final cartItems = [];
   String emailAddress;
   String password;
+  String description;
+  String reference;
   PageAction _currentAction = PageAction();
   PageAction get currentAction => _currentAction;
 
