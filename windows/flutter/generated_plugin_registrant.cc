64662a435d197960ca352f1776516abbda46eeb5
Yair Telezon <yair.telezon@socialpath.com.ar>
2023-03-22 11:00:59 -0300

    Fix in pubspec.yaml, updated dio dependency for security reasons

---
+++ windows/flutter/generated_plugin_registrant.cc
@@ -0,0 +1,29 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#include "generated_plugin_registrant.h"
+
+#include <connectivity_plus_windows/connectivity_plus_windows_plugin.h>
+#include <file_selector_windows/file_selector_windows.h>
+#include <flutter_barcode_sdk/flutter_barcode_sdk_plugin.h>
+#include <native_pdf_renderer/native_pdf_renderer_plugin.h>
+#include <url_launcher_windows/url_launcher_windows.h>
+#include <window_size/window_size_plugin.h>
+
+void RegisterPlugins(flutter::PluginRegistry* registry) {
+  ConnectivityPlusWindowsPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("ConnectivityPlusWindowsPlugin"));
+  FileSelectorWindowsRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("FileSelectorWindows"));
+  FlutterBarcodeSdkPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("FlutterBarcodeSdkPlugin"));
+  NativePdfRendererPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("NativePdfRendererPlugin"));
+  UrlLauncherWindowsRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("UrlLauncherWindows"));
+  WindowSizePluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("WindowSizePlugin"));
+}
